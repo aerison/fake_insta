a160c72aadabb100f1cfb5515c02924817e915f8
aerison <sonsoo1224@gmail.com>
2018-06-27 06:22:46 +0000

    listup

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,37 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user! , except: [:index]
   def index
-    @posts = Post.all
+    @posts = Post.order(created_at: :desc).page(params[:page]).per(5)
+    respond_to do |format|
+      format.html
+      format.json{render json: @posts}
+    end
   end
 
   def new
+    @post = Post.new
   end
 
   def create
-    @post = Post.new(post_params)
-    @post.save
-    redirect_to "/"
+    @post = current_user.posts.new(post_params)
+    respond_to do |format|
+      #format.html{render :new}
+      #format.json{render json: @posts}
+    if @post.save
+        #저장이 되었을 경우에 실행
+      #flash[:notice]="글 작성이 완료되었습니다."
+        #redirect_to "/"
+        format.html{redirect_to '/', notice:'success'}
+    else
+      #저장이 실패했을경우에(validation) 걸렸을 때 실행
+      # flash[:alert]="글 작성이 실패하였습니다."
+      # redirect_to new_post_path
+      format.html{render :new}
+      format.json{render json: @post.errors}
+    end
+  end
+
   end
 
   def show
@@ -22,7 +43,7 @@ class PostsController < ApplicationController
 
   def update
     @post.update(post_params)
-    redirect_to "posts/#{@post.id}"
+    redirect_to "/posts/#{@post.id}"
   end
 
   def destroy
@@ -36,6 +57,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.permit(:title, :content)
+    params.require(:post).permit(:title, :content)
   end
 end
